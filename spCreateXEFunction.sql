0914ca590c67e1566c9b2e0cf80323193f8b9855
Sue Jennings <139395675+suejen566@users.noreply.github.com>
2024-03-28 18:59:46 -0400

    Add files via upload

--- spCreateXEFunction.sql
+++ spCreateXEFunction.sql
@@ -1,11 +1,11 @@
 /*
 **	Create a table-valued function to return shredded Extended Event target data.  
-**	Currently, only ring_buffer target types are supported.
 */
 CREATE	PROCEDURE spCreateXEFunction
 				@SessionName		sysname,					-- Name of existing Extended Event session
 				@FunctionName		sysname,					-- Name of function to create or overwrite
 				@RecreateFunction	tinyint = 0,				-- Whether existing @FunctionName should be overwritten
+				@Target				char(1) = 'R',				-- see @Target validation below
 				@EventNames			nvarchar(max) = NULL,		-- Optional list of Extended Events to query; NULL = ALL
 				@ColumnOrder		char(1) = 'A',				-- C = order by most Common columns first; <> C = order columns Alphabetically
 				@NamingConvention	char(1) = 'P'				-- see @Naming validation below
@@ -21,6 +21,8 @@ BEGIN
 				SELECT	@RecreateFunction = 0
 		IF		@NamingConvention NOT IN ('P', 'C', 'S')
 				RAISERROR('Invalid @NamingConvention:  P=PascalCase, C=camelCase, S=snake_case', 16, 1)
+		if		@Target NOT IN ('R', 'F')
+				RAISERROR('Invalid @Target: R=ring_buffer, F=event_file', 16, 1)
 
 		/*
 		**	Create temporary table to convert Extended Event to SQL datatype. Types are ordered by compatibility.
@@ -137,31 +139,23 @@ BEGIN
 								WHERE	c2.ColName = c1.ColName
 							ORDER BY	t.TypeID)
 				FROM	@EventColumns c1;
+		DROP	TABLE IF EXISTS ##e
+		SELECT	* INTO ##e FROM @EventColumns
 
 		DECLARE	@ResultColumns	TABLE (
-				ColName			sysname,
-				NodeName		varchar(8),
-				Element			varchar(8),
-				DataType		sysname,
 				DisplayName		sysname,
-				Events			nvarchar(max),
-				NumEvents		int,
+				DataType		sysname,
 				XSMethod		nvarchar(max),
 				ColOrder		int);
 
 		/*
 		**	Build a XQuery string for each distinct column name.  If the column name is included in 
 		**	multiple events, it's possible it could be both a global field and event field, in which
-		**	case we query all event node names.  The EventNames are strung together and counted so
-		**	we can avoid calling the xml method if the column isn't included in the event. 
+		**	case we query the nodes conditinally depending on the event.  The EventNames are strung together
+		**	and counted so we can avoid calling the xml method if the column isn't included in the event. 
 		*/
 		WITH	cteColumnDef AS (
-				SELECT	ColName, 
-						CASE	STRING_AGG(NodeName, ',')
-								WHEN 'action'	THEN 'action'
-								WHEN 'data'		THEN 'data'
-												ELSE '*'
-						END NodeName, 
+				SELECT	ColName, NodeName,
 						STRING_AGG(''''+EventName+'''', ',') Events,
 						COUNT(*) NumEvents,
 						Element, DataType,
@@ -174,25 +168,28 @@ BEGIN
 									FROM	(SELECT value, ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) Row 
 												FROM STRING_SPLIT(ColName, '_')) nc)))) DisplayName
 						FROM	@EventColumns 
-					GROUP BY	ColName, Element, DataType)
-				INSERT	INTO @ResultColumns
-						SELECT	ColName, NodeName, Element, DataType, DisplayName, Events, NumEvents,
+					GROUP BY	ColName, Element, DataType, NodeName),
+				cteConsolidateCols AS (
+						SELECT	ColName, NodeName, Element, DataType, DisplayName, Events, 
+								IIF(NumEvents <> @NumEvents, 1, 0) Conditional,
 								CONVERT(nvarchar(max), 
 									CASE	DataType
 											WHEN 'xml'				THEN CONCAT('e.query(''event/', NodeName, '[@name="', ColName, '"]/value/*'') ')
 											WHEN 'varbinary(max)'	THEN CONCAT('e.value(''xs:hexBinary((event/', NodeName, '[@name="', ColName, '"]/value)[1])'', ''', DataType, ''') ')
 																	ELSE CONCAT('e.value(''(event/', NodeName, '[@name="', ColName, '"]/', Element, ')[1]'', ''', DataType, ''') ')
 									END)	XSMethod,
-								ROW_NUMBER() OVER (ORDER BY IIF(@ColumnOrder = 'C', NumEvents, 0) DESC, ColName) ColOrder
-						FROM	cteColumnDef
-		/*
-		**	Eliminate calls of the xml method if the column doesn't exist for the event.  
-		**	This provides a slight performance improvement; with xml queries, any little bit helps!
-		*/
-		UPDATE	@ResultColumns
-				SET		XSMethod = CONCAT('IIF(EventName NOT IN (', Events, '), NULL, ', XSMethod, ')')
-				WHERE	NumEvents <> @NumEvents
-
+								DENSE_RANK() OVER (ORDER BY IIF(@ColumnOrder = 'C', NumEvents, 0) DESC, ColName) ColOrder								
+								FROM	cteColumnDef)
+				INSERT	INTO @ResultColumns (DisplayName, DataType, XSMethod, ColOrder)
+						SELECT	DisplayName, DataType,
+								CONCAT(IIF(Conditional = 0, '', 'CASE '),
+										STRING_AGG(IIF(Conditional = 0, '', 'WHEN EventName IN ('+Events+') THEN ')+XSMethod+'
+										', ''),
+										IIF(Conditional = 0, '', 'END')),
+								ColOrder
+								FROM	cteConsolidateCols
+							GROUP BY	DisplayName, DataType, Conditional, ColOrder
+								
 		DROP TABLE IF EXISTS ##c
 		SELECT	* INTO ##c FROM @ResultColumns
 
@@ -204,7 +201,12 @@ BEGIN
 		/*
 		**	Create the ddl for the function and execute it.
 		*/
-		SELECT	@Stmt = CONCAT(IIF(@RecreateFunction = 1, 'ALTER', 'CREATE'), ' FUNCTION ', @FunctionName, '()
+		SELECT	@Stmt = CONCAT(IIF(@RecreateFunction = 1, 'ALTER', 'CREATE'), ' FUNCTION ', @FunctionName, '(', 
+				IIF(@Target = 'R', '', '
+				@Path															nvarchar(260),
+				@MDPath															nvarchar(260),
+				@InitialFileName												nvarchar(260),
+				@InitialOffset													bigint'), ')
 RETURNS @EventsRet TABLE (
 				EventTime                                                       datetime,
 				EventName                                                       sysname,', 
@@ -212,25 +214,39 @@ RETURNS @EventsRet TABLE (
 				', CONVERT(nvarchar(max), DisplayName), REPLICATE(' ', 64 - LEN(DisplayName)), DataType), ',') 
 					WITHIN GROUP (ORDER BY ColOrder), ') AS
 BEGIN
-
 		DECLARE	@TargetData		xml
-
-		SELECT	@TargetData = CONVERT(xml, t.target_data)
+		', IIF(@Target = 'F', 
+				'SELECT	@TargetData = (
+						SELECT	CONVERT(xml, event_data)
+								FROM	sys.fn_xe_file_target_read_file(@Path, @MDPath, @InitialFileName, @InitialOffset)
+									FOR	XML PATH)',
+				CONCAT('SELECT	@TargetData = CONVERT(xml, t.target_data)
 				FROM	sys.dm_xe_session_targets t
 				JOIN	sys.dm_xe_sessions s 
 					ON	s.address = t.event_session_address
 				WHERE	s.name = ''', @SessionName, '''
-					AND	t.target_name = ''ring_buffer''
+					AND	t.target_name = ''ring_buffer''')
+				)/*ENDIIF*/, '
+		DECLARE	@Events		TABLE (
+				Event		xml)
+		INSERT	INTO @Events 
+				', IIF(@Target = 'F', 'SELECT	CONVERT(xml, event_data)
+						FROM	sys.fn_xe_file_target_read_file(@Path, @MDPath, @InitialFileName, @InitialOffset)',
+						'SELECT	e.query(''.'')
+						FROM	@TargetData.nodes(''RingBufferTarget/event'') Event(e)')/*ENDIIF*/, '
 
 		INSERT	INTO @EventsRet
-				SELECT	e.value(''(event/@timestamp)[1]'', ''datetime''),
-						e.EventName,', STRING_AGG(CONCAT('
-						', XSMethod, ' ', DisplayName), ',') WITHIN GROUP (ORDER BY ColOrder), '
+				SELECT	EventTime,
+						EventName,', STRING_AGG(CONCAT('
+						',XSMethod, ' ', DisplayName), ',') WITHIN GROUP (ORDER BY ColOrder), '
 						FROM	(
-								SELECT	e.value(''(@name)[1]'', ''sysname'') EventName, e.query(''.'') e
-										FROM	@TargetData.nodes(''RingBufferTarget/event'') Event(e)', IIF(@EventNames IS NULL, '', '
-										WHERE	e.value(''(@name)[1]'', ''sysname'') IN ('+
-						(SELECT STRING_AGG('''' + value + '''', ',') FROM STRING_SPLIT(@EventNames, ','))+')'), ' ) e
+								SELECT	Event.value(''(@timestamp)[1]'', ''datetime'') EventTime,
+										Event.value(''(@name)[1]'', ''sysname'') EventName, 
+										Event.query(''.'') e
+										FROM	@TargetData.nodes(''', IIF(@Target = 'F', 'row', 'RingBufferTarget'), '/event'') Target(Event)',
+						IIF(@EventNames IS NULL, '', '
+										WHERE	Event.value(''(event/@name)[1]'', ''sysname'') IN ('+
+								(SELECT STRING_AGG('''' + value + '''', ',') FROM STRING_SPLIT(@EventNames, ','))+')')/*ENDIIF*/, ') Events 
 
 		RETURN
 END')			FROM	@ResultColumns
@@ -248,6 +264,7 @@ END')			FROM	@ResultColumns
 								'; State: ',		ERROR_STATE(),
 								'; Procedure: ',	ERROR_PROCEDURE(),
 								'; Line: ',			ERROR_LINE())
-		PRINT	(@ErrorMsg)
+		PRINT	(@ErrorMsg);
+		THROW;
 	END CATCH;
 END
